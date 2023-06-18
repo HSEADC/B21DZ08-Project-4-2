12cf42a4f6b39adc235b46170d85e92991e4442b
кариночка <rin.ramazanova@yandex.com>
2023-06-18 11:11:04 +0300

    меню мобилка

--- src/components/A_MenuMobileToggle/A_MenuMobileToggle.jsx
+++ src/components/A_MenuMobileToggle/A_MenuMobileToggle.jsx
@@ -2,18 +2,24 @@ import React from 'react';
 import './A_MenuMobileToggle.scss'
 import classnames from 'classnames'
 
-
 export default class A_MenuMobileToggle extends React.Component {
-    render() {
-
-        const { url } = this.props
+  handleClick = () => {
+    const searchbb = document.querySelector(".M_SearchBar")
+    const menuelems = document.querySelector(".M_MenuElements")
+    console.log('clockclickkk');
+    searchbb.classList.toggle("Shows")
+    menuelems.classList.toggle("Shows")
     
-        return (
-          <a className='A_MenuMoblieToggle'>
-            <div></div>
-            <div></div>
-            <div></div>
-          </a>
-        )
-    }
+
+  };
+
+  render() {
+    return (
+      <a className='A_MenuMobileToggle' onClick={this.handleClick}>
+        <div></div>
+        <div></div>
+        <div></div>
+      </a>
+    );
   }
+}
