c2556c0a123294d5489b83bd08d2376791209b38
Vanurina Masha <90695332+Redvence@users.noreply.github.com>
2023-06-22 03:14:10 +0300

    доверстана о нас

--- src/web-taro/htmlgenerate.py
+++ src/web-taro/htmlgenerate.py
@@ -111,6 +111,9 @@ for i in range(len(page_names)):
     <analytics></analytics>
 
     <link rel="shortcut icon" href="../images/icon.png" />
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;400&display=swap" rel="stylesheet">
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   </head>
   <body>
